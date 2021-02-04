f31cd8f7e72643ac6d01a788bd6a18f1b4f13799
Quinn Butler <qeb@usf.edu>
2021-02-03 21:55:18 -0500

    Commit to add comments for assignment

--- butlerQ_Deliverable3_Array/Program.cs
+++ butlerQ_Deliverable3_Array/Program.cs
@@ -2,10 +2,31 @@
 
 namespace butlerQ_Deliverable3_Array
 {
+    /*
+    Author: Quinn Butler
+    Date: 2/5/2021
+    Comments: C# console application that creates an array, adds 25 integers in array, and ends application
+    */
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+            Watch the video posted in CANVAS on: Arrays
+            Review the source codes on the GitHub link. Then write the following program:
+            PROGRAM #1: Create a C# console application that demonstrates an Array with the following instructions:
+            1. Choose either type of array (Manually populated for Auto populated) seen in the example
+            2. The program doesn't ask the user anything, it will just run and execute code. No user input.
+            3. Have the program execute an array that is of size 25 elements.
+            4. Then, have the code simply display in the Console the following 25 times (Here is a sample of the output):
+            Element value = 1
+            Element value = 2
+            Element value = 3
+            .......... and so on .....
+            Element value = 25
+            Once you complete this program, upload them to GitHub.
+            Then, paste your 2 GitHub Link to GitHub here.
+             */
             //Declare array that we're populating
             int[] intArray = new int[25];
             //Declare count variable
