c05f0e64330370dfeca33f65565cfa43b938c6eb
Quinn Butler <qeb@usf.edu>
2021-02-03 21:23:03 -0500

    Commit code to repo

--- butlerQ_Deliverable3_Array/Program.cs
+++ butlerQ_Deliverable3_Array/Program.cs
@@ -6,7 +6,23 @@ namespace butlerQ_Deliverable3_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declare array that we're populating
+            int[] intArray = new int[25];
+            //Declare count variable
+            int count;
+            //Use iterative for loop for adding +1 to count very step and adds that value to the int Array
+            for (count = 0; count < intArray.Length; count++)
+            {
+                intArray[count] = count+1;
+                //Prints value for value that was added
+                Console.WriteLine("Element value = " + (count+1));
+            }
+            //print text for program ending
+            Console.WriteLine("Program end");
+            //For this case, program does print text in console and ends, but for troubleshooting the last two lines can be deleted to confirm that the lines
+            //print correctly
+            //Console.WriteLine("Please press any key to exit console.");
+            //Console.ReadKey(true);
         }
     }
 }
